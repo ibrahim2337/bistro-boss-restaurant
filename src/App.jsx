7b4aa42be04b1be1ddcba05fd06c365b4fa2acca
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-25 00:02:52 +0600

    footer section compleate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
