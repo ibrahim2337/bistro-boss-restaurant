8ce36dfb645132070708643f862ba723f22db017
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-05-25 17:35:16 +0600

    cetegory Section full ready

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,9 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import slide1 from "../../assets/home/slide1.jpg";
+import slide2 from "../../assets/home/slide2.jpg";
+import slide3 from "../../assets/home/slide3.jpg";
+import slide4 from "../../assets/home/slide4.jpg";
+import slide5 from "../../assets/home/slide5.jpg";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
 
 const Category = () => {
   return (
-    <div>
+    <div className="container mx-auto px-20">
       <div className=" text-center mt-20">
         <p className=" text-center text-xl text-yellow-400 mb-2">
           ---From 11:00am to 10:00pm---
@@ -12,14 +23,42 @@ const Category = () => {
       </div>
       <div className=" text-center">
         <p className="text-3xl mt-3 font-bold mb-4">ORDER ONLINE </p>
-        <p className="mx-auto border-b-4 w-80"></p>
+        <p className="mx-auto border-b-4 w-80 mb-20"></p>
       </div>
 
-      <div>
-        
+      <div className="">
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={slide1} />
+            <h3 className="text-center text-3xl font-bold text-white -mt-20 uppercase ">salad</h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide2} />
+            <h3 className="text-center text-3xl font-bold text-white -mt-20 uppercase ">pizzas</h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide3} />
+            <h3 className="text-center text-3xl font-bold text-white -mt-20 uppercase ">soups</h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide4} />
+            <h3 className="text-center text-3xl font-bold text-white -mt-20 uppercase ">desserts</h3>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide5} />
+            <h3 className="text-center text-3xl font-bold text-white -mt-18 uppercase ">salad</h3>
+          </SwiperSlide>
+        </Swiper>
       </div>
-
-
     </div>
   );
 };
